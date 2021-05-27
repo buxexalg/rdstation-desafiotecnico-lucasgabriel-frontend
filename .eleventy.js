9a88f2c966bbd6de721e440c656703494c113b4e
lucasgralcantara <lucas.alcantara@cubos.io>
2021-05-27 00:45:18 -0300

    feat: add git ignore and js compatibility

--- .eleventy.js
+++ .eleventy.js
@@ -1,14 +1,27 @@
+const del = require('del');
+const path = require("path");
+
+const config = {
+	passthroughFileCopy: true,
+	dir: {
+		input: "src",
+		output: "_site"
+	},
+	templateFormats: ["njk", "html", "png", "json", "svg", "js"]
+};
+
+const dirToClean = path.join(config.dir.output, "*");
+del.sync(dirToClean, { dot: true });
+
 module.exports = function (eleventyConfig) {
+
+	eleventyConfig.setUseGitIgnore(false);
+
+	eleventyConfig.addPassthroughCopy("./src/**/*.js")
 
 	eleventyConfig.addPassthroughCopy({
 		"./src/_includes/compiledStyles/main.css": "./main.css"
 	});
 
-	return {
-		dir: {
-			input: "src",
-			output: "_site"
-		},
-		templateFormats: ["njk", "md", "html"]
-	}
+	return config;
 }
