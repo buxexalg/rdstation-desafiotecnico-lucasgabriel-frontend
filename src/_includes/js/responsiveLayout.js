cf03b36d38459715e833da58e39fa3fb1a71a5db
buxexalg <lucasgralcantara@gmail.com>
2021-05-29 23:42:51 -0300

    feat: add responsiveLayout functions

---
+++ src/_includes/js/responsiveLayout.js
@@ -0,0 +1,4 @@
+const freeAccountButton = document.querySelector(".freeAccountButton");
+const freeAccountSection = document.querySelector(".freeAccountSection");
+
+freeAccountSection.append(freeAccountButton);
